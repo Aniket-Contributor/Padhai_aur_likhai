a97e871e23b869a5a62cea6735afdda7d9fbab10
Aniket-Contributor <115075930+Aniket-Contributor@users.noreply.github.com>
2024-07-21 18:48:50 +0530

    Update axiosInstance.js

--- client/src/Helper/axiosInstance.js
+++ client/src/Helper/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1";
+const BASE_URL = "https://padhai-aur-likhai-2.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
